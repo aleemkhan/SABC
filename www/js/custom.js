132a1db4eb951342874384e8b5cb314a01b336d3
Muhammad Amsal <MuhammadAmsal@MacBook-Pro.local>
2016-05-27 20:09:21 +0500

    first commit

---
+++ www/js/custom.js
@@ -0,0 +1,11 @@
+function fbClick(){
+	console.log("Call fb functions.");
+}
+
+function instaClick(){
+	console.log("Call insta functions.");
+}
+
+function twitterClick(){
+	console.log("Call twitter functions.");
+}
